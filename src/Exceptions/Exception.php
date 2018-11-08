43989c3e0ae00084e0c921eb800e26a653e92e95
dcynsd <dcynsd@gmail.com>
2018-11-08 14:54:36 +0800

    first commit

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Dcynsd\Weather\Exceptions;
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Dcynsd\Weather\Exceptions;
+
+class InvalidArgumentException extends Exception
+{
+
+}
